238ac7b779f4072aab663e6c14d1bf740b04ff88
ohlulu <ohlulu@goonsdesign.com>
2019-09-01 12:42:04 +0800

    [add] [view]

--- AccuWeatherDemo/AccuWeatherDemo/ViewController.swift
+++ AccuWeatherDemo/AccuWeatherDemo/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var tmpLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
